d5255bdd4132a56812b8a7b6f6d163846a41848a
faketuna <faketuna.st@gmail.com>
2023-02-16 18:03:35 +0900

    Update simpleGit.bat

--- simpleGit.bat
+++ simpleGit.bat
@@ -1,4 +1,10 @@
+@echo off
+
+echo ****Deleting docs directory****
 rd /Q /S docs
+echo ****Building app****
 call npm run build
+echo ****Rename the build directory to docs directory****
 move D:\\myworks\\github\\website\\faketuna.github.io\\app\\build D:\\myworks\\github\\website\\faketuna.github.io\\app\\docs
+echo ****Git****
 git add . && git commit && git push origin development
